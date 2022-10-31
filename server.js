5df6a6ff9932e64cb9e7d365113d8bf149d1aca6
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-31 16:19:47 -0400

    create .env-sample

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_URL);
 
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
